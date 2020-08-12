5a18074f63701b3493687dc9e1ee3c70d1cae229
emerferri <emersonferri.pb@gmail.com>
2020-08-12 10:11:10 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/ferri/com/ghips/MainActivity.kt
@@ -0,0 +1,6 @@
+package ferri.com.ghips
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ghips/ui/gif_page.dart';
+import 'package:ghips/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white),
+  ));
+}
